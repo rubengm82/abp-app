93df2c40530303a7281a3eb4d5e0f7bb69dbf095
ruben <ruben.gallardo@inslapineda.cat>
2025-10-07 12:30:42 +0200

    Instalacion de TailWind

--- resources/views/center/centerForm.blade.php
+++ resources/views/center/centerForm.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Afegir centre</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <form action="{{ route("center_add") }}" method="post">
