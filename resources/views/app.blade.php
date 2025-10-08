ee0fd098ced1a1603b57339d7903225de7f5826e
ruben <ruben.gallardo@inslapineda.cat>
2025-10-09 00:33:36 +0200

    Añadido FontAwesome-Icons al proyecto y funcion del menu-user de topbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Valparadis App')</title>
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('css/fontawesome701.css') }}">
 </head>
 <body class="bg-gray-100">
 
