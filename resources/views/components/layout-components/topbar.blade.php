34c742ac0e983b25143573283f1a13baa32055d6
ruben <ruben.gallardo@inslapineda.cat>
2025-10-22 14:13:10 +0200

    Mejorada la posicion de llos elements como donde se posicona en nombre, el centro, el rol en topbar.

--- resources/views/components/layout-components/topbar.blade.php
+++ resources/views/components/layout-components/topbar.blade.php
@@ -4,9 +4,7 @@
     <!-- Logo  -->
     <div class="flex items-center text-lg font-bold select-none">
         <a href="{{ route('home') }}" class="block px-4 py-2 no-underline">
-            <span class="text-[#ff7500]">{{ Auth::user()->role ?? 'Sense Rol' }}</span>
-            <span class="text-[#fafafa]">- {{ (Auth::user()->center)->name}}</span>
-            <span class="text-[#fafafa]">({{ (Auth::user()->name) . ' ' . (Auth::user()->surname1) . ' ' . (Auth::user()->surname2 ?? '') }})</span>
+            <span class="text-primary text-3xl">{{ (Auth::user()->center)->name}}</span>
         </a>
     </div>
 
@@ -20,11 +18,27 @@
 
         <!-- Menu -->
         <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 mt-2 z-20">
+            
             <!-- Version APP -->
-            <li class="text-gray-400 pointer-events-none">
-                <span class="pl-2 pr-2 w-full block text-right cursor-default">Sprint 2 | Versió 0.3.0</span>
+            <li class="text-gray-400 pointer-events-none pr-2">
+                <span class=" w-full block text-right cursor-default">Sprint 3 | Versió 0.3.0</span>
             </li>
 
+            <!-- Separator -->
+            <span class="block border-t border-gray-400 my-2 w-full rounded-none"></span>
+            
+            <li class="text-gray-400 pointer-events-none flex flex-col items-center gap-1">
+                <span class="text-base-100 font-bold dark:text-base-content text-center">
+                    {{ (Auth::user()->name) . ' ' . (Auth::user()->surname1) . ' ' . (Auth::user()->surname2 ?? '') }}
+                </span>
+                <span class="text-primary font-bold text-center text-xl">
+                    {{ Auth::user()->role ?? 'Sense Rol' }}
+                </span>
+            </li>
+            
+            <!-- Separator -->
+            <span class="block border-t border-gray-400 my-2 w-full rounded-none"></span>
+            
             <!-- Dark/Light Mode Toggle -->
             <li class="p-2">
                 <div class="flex items-center justify-between w-full">
@@ -37,6 +51,9 @@
                     />
                 </div>
             </li>
+
+            <!-- Separator -->
+            <span class="block border-t border-gray-400 my-2 w-full rounded-none"></span>
 
             <!-- Logout -->
             <li>
