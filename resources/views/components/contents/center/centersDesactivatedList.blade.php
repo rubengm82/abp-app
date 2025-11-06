0561355c14ed50ad677ca8c7f1230a770741027f
ruben <ruben.gallardo@inslapineda.cat>
2025-11-06 12:46:43 +0100

    Fix SVG to x-Icons en pagainas Desactivados

--- resources/views/components/contents/center/centersDesactivatedList.blade.php
+++ resources/views/components/contents/center/centersDesactivatedList.blade.php
@@ -29,9 +29,9 @@
     @else
         <div class="text-center py-12">
             <div class="text-base-content/50 text-lg mb-4">
-                <svg class="w-16 h-16 mx-auto mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
-                </svg>
+                <div class="flex justify-center mb-4">
+                    <x-partials.icon name="building-office" class="w-15 h-15 text-primary" />
+                </div>
             </div>
             <h3 class="text-xl font-semibold text-base-content mb-2">No hi ha centres desactivats</h3>
             <p class="text-base-content/70 mb-4">Tots els centres estan actualment actius.</p>
--- resources/views/components/contents/projectcommission/projectCommissionsDesactivatedList.blade.php
+++ resources/views/components/contents/projectcommission/projectCommissionsDesactivatedList.blade.php
@@ -29,9 +29,9 @@
     @else
         <div class="text-center py-12">
             <div class="text-base-content/50 text-lg mb-4">
-                <svg class="w-16 h-16 mx-auto mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
-                </svg>
+                <div class="flex justify-center mb-4">
+                    <x-partials.icon name="rectangle-group" class="w-15 h-15 text-primary" />
+                </div>
             </div>
             <h3 class="text-xl font-semibold text-base-content mb-2">No hi ha projectes/comissions desactivats</h3>
             <p class="text-base-content/70 mb-4">Tots els projectes i comissions estan actualment actius.</p>
