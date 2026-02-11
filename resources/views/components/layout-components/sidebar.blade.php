ee2a95b502c90807e31355d6d68acfbf78b1acf7
Luipy56 <yoelberjaga@gmail.com>
2026-02-11 17:00:25 +0100

    Sidebar: nou ordre 1-13 i nou apartat Estoc de Material

--- resources/views/components/layout-components/sidebar.blade.php
+++ resources/views/components/layout-components/sidebar.blade.php
@@ -104,7 +104,169 @@
                 </details>
             </li>
 
-            <!-- 2. Serveis Generals -->
+            <!-- 2. Cursos -->
+            <li>
+                <details>
+                    <summary class="font-normal text-base-content">
+                        <x-partials.icon name="academic-cap" class="w-6 h-6 text-primary" />
+                        Cursos
+                    </summary>
+                    <ul class="text-xs text-base-content/65">
+                        <li>
+                            <a href="{{ route('courses_list') }}">
+                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
+                                Llistar
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="text-xs text-base-content/65">
+                        <li>
+                            <a href="{{ route('course_form') }}">
+                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
+                                Afegir
+                            </a>
+                        </li>
+                    </ul>
+                </details>
+            </li>
+
+            <!-- 3. Projectes/Comissions -->
+            <li>
+                <details>
+                    <summary class="font-normal">
+                        <x-partials.icon name="rectangle-group" class="w-6 h-6 text-primary" />
+                        Projectes/Comissions
+                    </summary>
+                    <ul class="text-xs text-base-content/65">
+                        <li>
+                            <a href="{{ route('projectcommissions_list') }}">
+                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
+                                Llistar
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="text-xs text-base-content/65">
+                        <li>
+                            <a href="{{ route('projectcommission_form') }}">
+                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
+                                Afegir
+                            </a>
+                        </li>
+                    </ul>
+                </details>
+            </li>
+
+            <!-- 4. Registre uniformitat -->
+            @if(in_array(Auth::user()->permissions ?? null, ['Direcció', 'Administració', 'Gerència']))
+            <li>
+                <details>
+                    <summary class="font-normal">
+                        <x-partials.icon name="identification" class="w-6 h-6 text-primary" />
+                        Registre uniformitat
+                    </summary>
+                    <ul class="text-xs text-base-content/65">
+                        <li>
+                            <a href="{{ route('materialassignments_list') }}">
+                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
+                                Llistar
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('materialassignments_existencies_roba_list') }}">
+                                <x-partials.icon name="tag" class="w-4 h-4 text-info" />
+                                Existencies Roba
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('materialassignment_form') }}">
+                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
+                                Afegir
+                            </a>
+                        </li>
+                    </ul>
+                </details>
+            </li>
+            @endif
+
+            <!-- 5. Temes pendents amb RRHH -->
+            @if((Auth::user()->permissions ?? null) === 'Direcció' || (Auth::user()->permissions ?? null) === 'Gerència')
+            <li>
+                <details>
+                    <summary class="font-normal">
+                        <x-partials.icon name="exclamation-triangle" class="w-6 h-6 text-primary" />
+                        Temes pendents amb RRHH
+                    </summary>
+                    <ul class="text-xs text-base-content/65">
+                        <li>
+                            <a href="{{ route('hr_issues_list') }}">
+                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
+                                Llistar
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="text-xs text-base-content/65">
+                        <li>
+                            <a href="{{ route('hr_issue_form') }}">
+                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
+                                Afegir
+                            </a>
+                        </li>
+                    </ul>
+                </details>
+            </li>
+            @endif
+
+            <!-- 6. Contactes Externs -->
+            <li>
+                <details>
+                    <summary class="font-normal">
+                        <x-partials.icon name="phone" class="w-6 h-6 text-primary" />
+                        Contactes Externs
+                    </summary>
+                    <ul class="text-xs text-base-content/65">
+                        <li>
+                            <a href="{{ route('externalcontacts_list') }}">
+                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
+                                Llistar
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="text-xs text-base-content/65">
+                        <li>
+                            <a href="{{ route('externalcontact_form') }}">
+                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
+                                Afegir
+                            </a>
+                        </li>
+                    </ul>
+                </details>
+            </li>
+
+            <!-- 7. Serveis complementaris -->
+            <li>
+                <details>
+                    <summary class="font-normal">
+                        <x-partials.icon name="wrench-screwdriver" class="w-6 h-6 text-primary" />
+                        Serveis complementaris
+                    </summary>
+                    <ul class="text-xs text-base-content/65">
+                        <li>
+                            <a href="{{ route('complementaryservices_list') }}">
+                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
+                                Llistar
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('complementaryservice_form') }}">
+                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
+                                Afegir
+                            </a>
+                        </li>
+                    </ul>
+                </details>
+            </li>
+
+            <!-- 8. Serveis Generals -->
             @if(in_array(Auth::user()->permissions ?? null, ['Direcció', 'Administració', 'Gerència']))
             <li>
                 <details>
@@ -136,83 +298,17 @@
             </li>
             @endif
 
-            <!-- 3. Serveis Complementaris -->
+            <!-- 9. Estoc de Material -->
+            @if(in_array(Auth::user()->permissions ?? null, ['Direcció', 'Administració', 'Gerència']))
             <li>
-                <details>
-                    <summary class="font-normal">
-                        <x-partials.icon name="wrench-screwdriver" class="w-6 h-6 text-primary" />
-                        Serveis Complementaris
-                    </summary>
-                    <ul class="text-xs text-base-content/65">
-                        <li>
-                            <a href="{{ route('complementaryservices_list') }}">
-                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
-                                Llistar
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('complementaryservice_form') }}">
-                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
-                                Afegir
-                            </a>
-                        </li>
-                    </ul>
-                </details>
+                <a href="{{ route('materialassignments_existencies_roba_list') }}">
+                    <x-partials.icon name="cube" class="w-6 h-6 text-primary" />
+                    Estoc de Material
+                </a>
             </li>
+            @endif
 
-            <!-- 4. Contactes Externs -->
-            <li>
-                <details>
-                    <summary class="font-normal">
-                        <x-partials.icon name="phone" class="w-6 h-6 text-primary" />
-                        Contactes Externs
-                    </summary>
-                    <ul class="text-xs text-base-content/65">
-                        <li>
-                            <a href="{{ route('externalcontacts_list') }}">
-                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
-                                Llistar
-                            </a>
-                        </li>
-                    </ul>
-                    <ul class="text-xs text-base-content/65">
-                        <li>
-                            <a href="{{ route('externalcontact_form') }}">
-                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
-                                Afegir
-                            </a>
-                        </li>
-                    </ul>
-                </details>
-            </li>
-
-            <!-- 4b. Cursos -->
-            <li>
-                <details>
-                    <summary class="font-normal text-base-content">
-                        <x-partials.icon name="academic-cap" class="w-6 h-6 text-primary" />
-                        Cursos
-                    </summary>
-                    <ul class="text-xs text-base-content/65">
-                        <li>
-                            <a href="{{ route('courses_list') }}">
-                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
-                                Llistar
-                            </a>
-                        </li>
-                    </ul>
-                    <ul class="text-xs text-base-content/65">
-                        <li>
-                            <a href="{{ route('course_form') }}">
-                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
-                                Afegir
-                            </a>
-                        </li>
-                    </ul>
-                </details>
-            </li>
-
-            <!-- 5. Manteniments -->
+            <!-- 10. Manteniments -->
             @if(in_array(Auth::user()->permissions ?? null, ['Direcció', 'Administració', 'Gerència']))
             <li>
                 <details>
@@ -240,71 +336,7 @@
             </li>
             @endif
 
-            <!-- 6. Temes pendents RRHH -->
-            @if((Auth::user()->permissions ?? null) === 'Direcció' || (Auth::user()->permissions ?? null) === 'Gerència')
-            <li>
-                <details>
-                    <summary class="font-normal">
-                        <x-partials.icon name="exclamation-triangle" class="w-6 h-6 text-primary" />
-                        Temes pendents RRHH
-                    </summary>
-                    <ul class="text-xs text-base-content/65">
-                        <li>
-                            <a href="{{ route('hr_issues_list') }}">
-                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
-                                Llistar
-                            </a>
-                        </li>
-                    </ul>
-                    <ul class="text-xs text-base-content/65">
-                        <li>
-                            <a href="{{ route('hr_issue_form') }}">
-                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
-                                Afegir
-                            </a>
-                        </li>
-                    </ul>
-                </details>
-            </li>
-            @endif
-
-            <!-- 7. Projectes/Comissions -->
-            <li>
-                <details>
-                    <summary class="font-normal">
-                        <x-partials.icon name="rectangle-group" class="w-6 h-6 text-primary" />
-                        Projectes/Comissions
-                    </summary>
-                    <ul class="text-xs text-base-content/65">
-                        <li>
-                            <a href="{{ route('projectcommissions_list') }}">
-                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
-                                Llistar
-                            </a>
-                        </li>
-                    </ul>
-                    <ul class="text-xs text-base-content/65">
-                        <li>
-                            <a href="{{ route('projectcommission_form') }}">
-                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
-                                Afegir
-                            </a>
-                        </li>
-                    </ul>
-                </details>
-            </li>
-
-            <!-- 8. Documents Globals -->
-            @if(in_array(Auth::user()->permissions ?? null, ['Direcció', 'Gerència']))
-            <li class="font-normal">
-                <a href="{{ route('global_documents_list') }}">
-                    <x-partials.icon name="document-text" class="w-6 h-6 text-primary" />
-                    Documents Globals
-                </a>
-            </li>
-            @endif
-
-            <!-- Al final: Centres -->
+            <!-- 11. Centre -->
             @if((Auth::user()->permissions ?? null) === 'Gerència')
             <li>
                 <details>
@@ -339,39 +371,17 @@
             </li>
             @endif
 
-            <!-- Al final: Registre de Uniformitat -->
-            @if(in_array(Auth::user()->permissions ?? null, ['Direcció', 'Administració', 'Gerència']))
-            <li>
-                <details>
-                    <summary class="font-normal">
-                        <x-partials.icon name="identification" class="w-6 h-6 text-primary" />
-                        Registre de Uniformitat
-                    </summary>
-                    <ul class="text-xs text-base-content/65">
-                        <li>
-                            <a href="{{ route('materialassignments_list') }}">
-                                <x-partials.icon name="queue-list" class="w-4 h-4 text-info" />
-                                Llistar
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('materialassignments_existencies_roba_list') }}">
-                                <x-partials.icon name="tag" class="w-4 h-4 text-info" />
-                                Existencies Roba
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('materialassignment_form') }}">
-                                <x-partials.icon name="plus" class="w-4 h-4 text-info" />
-                                Afegir
-                            </a>
-                        </li>
-                    </ul>
-                </details>
+            <!-- 12. Documents Globals -->
+            @if(in_array(Auth::user()->permissions ?? null, ['Direcció', 'Gerència']))
+            <li class="font-normal">
+                <a href="{{ route('global_documents_list') }}">
+                    <x-partials.icon name="document-text" class="w-6 h-6 text-primary" />
+                    Documents Globals
+                </a>
             </li>
             @endif
 
-            <!-- Submenu Desactivations -->
+            <!-- 13. Desactivacions -->
             <li>
                 <details>
                     <summary class="font-normal">
